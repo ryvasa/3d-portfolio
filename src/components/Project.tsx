fc11cf34f85b93e8578581a485a80f17358feb2f
ryvasa <ryanoktaviansaputra9@gmail.com>
2024-09-22 18:35:04 +0700

    edit

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,8 +1,8 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import { useScroll as useScrollHook } from "../libs/hooks/useSchroll";
-import { SectionContext } from "../libs/utils/context";
-import { LayoutGrid } from "./LayoutGrid";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { useContext, useEffect, useRef, useState } from 'react';
+import { useScroll as useScrollHook } from '../libs/hooks/useSchroll';
+import { SectionContext } from '../libs/utils/context';
+import { LayoutGrid } from './LayoutGrid';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import {
   Express,
   React,
@@ -14,7 +14,7 @@ import {
   Nest,
   Postgres,
   Socket,
-} from "./Icons";
+} from './Icons';
 
 const Project = (): JSX.Element => {
   const [isVisible, setIsVisible] = useState(false);
@@ -32,12 +32,12 @@ const Project = (): JSX.Element => {
 
   useEffect(() => {
     if (isVisible) {
-      dispatch({ section: "projects" });
+      dispatch({ section: 'projects' });
     }
   }, [isVisible, dispatch]);
   const { scrollYProgress } = useScroll({
     target: projectRef,
-    offset: ["start start", "end start"],
+    offset: ['start start', 'end start'],
   });
 
   const translateYToTop = useTransform(scrollYProgress, [0, 1], [0, -300]);
@@ -55,7 +55,7 @@ const Project = (): JSX.Element => {
           className="h-screen lg:w-4/5 w-[95%]"
           initial={!firstVisible && { opacity: 0 }}
           animate={firstVisible && { opacity: 1 }}
-          transition={{ duration: 1.0, ease: "easeInOut" }}
+          transition={{ duration: 1.0, ease: 'easeInOut' }}
         >
           <LayoutGrid cards={cards} />
         </motion.div>
@@ -72,9 +72,8 @@ const SkeletonOne = () => {
       <p className="font-bold md:text-4xl text-xl text-white">Online Store</p>
 
       <p className="font-normal text-base max-w-lg text-neutral-200">
-        This application is an online shop, as a visitor the user can see the
-        items being sold, if interested the user can also add them to the basket
-        and then place an order.
+        This application is an online shop, users can view items for sale, add
+        them to their cart, and also place orders.
       </p>
       <div className="flex flex-col gap-1">
         <p className="text-dark-xs font-semibold">Build with</p>
@@ -117,9 +116,9 @@ const SkeletonThree = () => {
     <div className="flex flex-col gap-1">
       <p className="font-bold md:text-4xl text-xl text-white">Sosmed App</p>
       <p className="font-normal text-base max-w-lg text-neutral-200">
-        This application has functionality like social media in general, such as
-        creating posts, and giving likes and comments, apart from that users can
-        also understand other users with the realtime chat feature.
+        This application has functions like social media in general, such as
+        making posts, and giving likes and comments, apart from that, users can
+        also connect with other users with the realtime chat feature.
       </p>
       <div className="flex flex-col gap-1">
         <p className="text-dark-xs font-semibold">Build with</p>
@@ -164,27 +163,27 @@ const cards = [
   {
     id: 1,
     content: <SkeletonOne />,
-    className: "lg:col-span-3 lg:row-span-4 col-start-3 col-span-2 row-span-3",
-    thumbnail: "/images/projects/home.png",
+    className: 'lg:col-span-3 lg:row-span-4 col-start-3 col-span-2 row-span-3',
+    thumbnail: '/images/projects/home.png',
   },
   {
     id: 2,
     content: <SkeletonTwo />,
-    className: "col-span-2 row-span-3 row-start-2 ",
-    thumbnail: "/images/projects/blog.png",
+    className: 'col-span-2 row-span-3 row-start-2 ',
+    thumbnail: '/images/projects/blog.png',
   },
   {
     id: 3,
     content: <SkeletonThree />,
     className:
-      "lg:col-span-3 lg:row-span-3 lg:row-start-5 col-span-2 row-span-4 col-start-3 row-start-4",
-    thumbnail: "/images/projects/sosmed.png",
+      'lg:col-span-3 lg:row-span-3 lg:row-start-5 col-span-2 row-span-4 col-start-3 row-start-4',
+    thumbnail: '/images/projects/sosmed.png',
   },
   {
     id: 4,
     content: <SkeletonFour />,
     className:
-      "lg:col-span-2 lg:row-span-3 lg:col-start-4 lg:row-start-5 col-span-2 row-span-3 row-start-5",
-    thumbnail: "/images/projects/dashboard.png",
+      'lg:col-span-2 lg:row-span-3 lg:col-start-4 lg:row-start-5 col-span-2 row-span-3 row-start-5',
+    thumbnail: '/images/projects/dashboard.png',
   },
 ];
